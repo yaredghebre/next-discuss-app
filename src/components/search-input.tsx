49eaf32020ac04c226b30d5412188810bef5e116
yaredghebre <y.ghebreghergis@futuretechlead.com>
2024-05-05 18:37:36 +0200

    logic complete, needs style OK

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,5 +1,23 @@
-import {Input} from "@nextui-org/react";
+"use client";
+
+import {Button, Input} from "@nextui-org/react";
+import {useSearchParams} from "next/navigation";
+import * as actions from "@/actions";
 
 export default function SearchInput() {
-  return <Input />;
+  const searchParams = useSearchParams();
+
+  return (
+    <form action={actions.search} className="flex gap-2 items-center">
+      <Input
+        name="term"
+        defaultValue={searchParams.get("term") || ""}
+        size="sm"
+        color="secondary"
+      />
+      <Button type="submit" color="secondary" variant="ghost" size="lg">
+        Search
+      </Button>
+    </form>
+  );
 }
