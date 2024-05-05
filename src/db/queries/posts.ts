cf6e8b95af1985d09e73be7160bcf61041da6ab3
yaredghebre <y.ghebreghergis@futuretechlead.com>
2024-05-05 16:24:18 +0200

    homepage OK

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,10 +7,6 @@ export type PostWithData = Post & {
   _count: { comments: number };
 };
 
-// export type PostWithData = Awaited<
-//   ReturnType<typeof fetchPostsByTopicSlug>
-// >[number];
-
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug } },
@@ -19,5 +15,21 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
       user: { select: { name: true } },
       _count: { select: { comments: true } },
     },
+  });
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: {
+      comments: {
+        _count: "desc",
+      },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
   });
 }
